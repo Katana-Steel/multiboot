d4290d0b138d63308bc38fd32b5a98e33e71f48e
Rene Kjellerup <rk.katana.steel@gmail.com>
2016-07-30 13:50:36 -0700

    building the main UI
    added a few of the essential ui components
    and added mock functions for enumerating available fs and devices

--- multiboot.py
+++ multiboot.py
@@ -7,11 +7,12 @@ import sys
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtWidgets import QDialog
 # from PyQt5.QtWidgets import QLineEdit
-# from PyQt5.QtWidgets import QLabel
-# from PyQt5.QtWidgets import QPushButton
+from PyQt5.QtWidgets import QComboBox
+from PyQt5.QtWidgets import QLabel
+from PyQt5.QtWidgets import QPushButton
 # from PyQt5.QtWidgets import QMessageBox
 from PyQt5.QtWidgets import QVBoxLayout
-# from PyQt5.QtWidgets import QHBoxLayout
+from PyQt5.QtWidgets import QHBoxLayout
 # from PyQt5.QtGui import QIntValidator
 # from PyQt5.QtCore import pyqtSlot
 
@@ -25,6 +26,40 @@ class MenuCreator(QDialog):
         s.setHeight(0)
         self.setMinimumSize(s)
         self.main = QVBoxLayout(self)
+        hlay = QHBoxLayout
+        self.main.addLayout(hlay)
+        hlay.addSpacer()
+        self.create = QPushButton('Create USB')
+        hlay.addWidget(self.create)
+        hlay.addSpacer()
+        hlay = QHBoxLayout
+        hlay.addWidget(QLabel('USB device: '))
+        self.dev = QComboBox()
+        self.getAvailableUSBDrives(self.dev)
+        hlay.addWidget(self.dev)
+        self.main.addLayout(hlay)        hlay = QHBoxLayout
+        hlay.addWidget(QLabel('Selected filesystem:'))
+        self.fs = QComboBox()
+        hlay.addWidget(self.fs)
+        self.main.addLayout(hlay)
+        self.getAvailableFilesystems(self.fs)
+        hlay = QHBoxLayout
+        hlay.addSpacing()
+        btn = QPushButton('add ISO File'
+        hlay.addWidget(QLabel('select filesystem'))
+        self.fs = QComboBox()
+        hlay.addWidget(self.fs)
+        self.main.addLayout(hlay)
+        
+    def getAvailableFilesystems(self, cbox):
+        # iterate over available mkfs.* programs
+        cbox.insertItem('ext4')
+
+
+    def getAvailableUSBDevices(self, dev):
+        # insert actual code for detecting
+        # usb flash storage
+        dev.insertItem('/dev/sdb')
 
     def __init__(self, parent=None):
         QDialog.__init__(self, parent)
