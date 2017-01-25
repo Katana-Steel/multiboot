7b7db160dac80e3631b31be673c0ea9dbfd44fcf
Rene Kjellerup <rene.kjellerup@nuance.com>
2017-01-25 12:47:20 -0800

    changed format test to request a virtual USB device we can format

--- tests/testformat.py
+++ tests/testformat.py
@@ -1,11 +1,11 @@
 import fs
 import sys
 
-my_USB = fs.linux.usbDev('/dev/null')
-print(my_USB.partitions())
+my_USB = fs.getFakeUsb()
+print('partitions before', my_USB.partitions())
 ext4fs = fs.linux.mkfs('ext4')
 my_USB.format(ext4fs, 0)
 if 'ext4' not in my_USB.partitions()[0][1]:
     print('format failed')
     sys.exit(1)
-print(my_USB.partitions())
+print('partitions after',my_USB.partitions())
